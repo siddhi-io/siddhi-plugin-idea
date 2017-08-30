92061eeb405592a8dd394af89d269893a956f34c
Raveen Savinda Rathnayake <raveen.s.r@gmail.com>
2017-08-30 09:59:12 +0530

    Added license header for header missed classes

--- src/main/java/org/wso2/plugins/idea/codeInsight/imports/SiddhiCodeInsightSettings.java
+++ src/main/java/org/wso2/plugins/idea/codeInsight/imports/SiddhiCodeInsightSettings.java
@@ -13,7 +13,6 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-
 package org.wso2.plugins.idea.codeInsight.imports;
 
 import com.intellij.openapi.components.*;
